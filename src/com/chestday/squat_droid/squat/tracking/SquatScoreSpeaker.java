bcce7f4bfe1dd7b00d8bef2476b2cf3b215ca99a
Jack Stevenson <cogwirrel@gmail.com>
2014-06-02 00:56:37 +0100

    Give some instant tips on your squat

--- src/com/chestday/squat_droid/squat/tracking/SquatScoreSpeaker.java
+++ src/com/chestday/squat_droid/squat/tracking/SquatScoreSpeaker.java
@@ -19,6 +19,20 @@ public class SquatScoreSpeaker {
 			text += "Perfect.";
 		}
 		
+		if(contributor.equals(SquatScorer.ABOVE_PARALLEL)) {
+			text += " Try squatting deeper.";
+		} else if(contributor.equals(SquatScorer.NO_LOCKOUT)) {
+			text += " Try to stand up straight at the top.";
+		} else if(contributor.equals(SquatScorer.BAD_BACK_ANGLE)) {
+			text += " Make sure your back isn't too far forward or backward.";
+		} else if(contributor.equals(SquatScorer.KNEES_FORWARD)) {
+			text += " Try not to let your knees go as far forward.";
+		} else if(contributor.equals(SquatScorer.WEIGHT_DISTRIBUTION_BACKWARD)) {
+			text += " Try to shift your weight forward.";
+		} else if(contributor.equals(SquatScorer.WEIGHT_DISTRIBUTION_FORWARD)) {
+			text += " Try to shift your weight backward.";
+		}
+		
 		Speaker.speak(text);
 	}
 }
